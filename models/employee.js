89857da26ea4c79b9f776a84541ea83e91ba78b1
Sinks <125329866+nicksinks@users.noreply.github.com>
2024-10-22 14:34:13 -0400

    changed the database queries in employee.js and employees.html

--- models/employee.js
+++ models/employee.js
@@ -2,7 +2,7 @@ const db = require('../db/connection');
 
 const Employee = {
     getAll: (callback) => {
-        db.query('SELECT * FROM employees', callback);
+        db.query('SELECT * FROM persons', callback);
     },
     add: (name, email, role, callback) => {
         db.query('INSERT INTO employees (name, email, role) VALUES (?, ?, ?)', [name, email, role], callback);
