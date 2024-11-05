abd21517351ad1682365994fa7204e1bd75e3ed4
Sinks <125329866+nicksinks@users.noreply.github.com>
2024-11-05 14:29:30 -0500

    deleted unimplemented class files.  added comments

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const db = require('../db/connection');
+const express = require('express'); // Import the express library
+const router = express.Router(); // Create a Router object
+const path = require('path'); // Import the path module
+const db = require('../db/connection'); // Import the connection object
 
+// Define the routes
 router.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
@@ -18,7 +19,7 @@ router.get('/employees', (req, res) => {
 router.get('/doors', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/doors.html'));
 });
-
+//get all employees by running a SQL select query
 router.get('/persons/list', (req, res) => {
         const query = 'SELECT * FROM persons';
         db.query(query, (err, results) => {
@@ -29,18 +30,18 @@ router.get('/persons/list', (req, res) => {
             res.json(results);  // Send the results as JSON
         });
     });
-
-    router.post('/persons/add', (req, res) => {
-        const { name, email, department, departmentID, secGroup } = req.body;
-        const query = 'INSERT INTO persons (name, email, department, departmentID, secGroup) VALUES (?, ?, ?, ?, ?)';
-        db.query(query, [name, email, department, departmentID, secGroup], (err, results) => {
-            if (err) {
-                console.error('Error adding employee:', err);
-                return res.status(500).json({ error: 'Failed to add employee' });
-            }
-            res.status(201).json({ message: 'Employee added successfully' });
-        });
+//add an employee by running a SQL insert query
+router.post('/persons/add', (req, res) => {
+    const { name, email, department, departmentID, secGroup } = req.body;
+    const query = 'INSERT INTO persons (name, email, department, departmentID, secGroup) VALUES (?, ?, ?, ?, ?)';
+    db.query(query, [name, email, department, departmentID, secGroup], (err, results) => {
+        if (err) {
+            console.error('Error adding employee:', err);
+            return res.status(500).json({ error: 'Failed to add employee' });
+        }
+        res.status(201).json({ message: 'Employee added successfully' });
     });
+});
 
 
 //update an employee by running a SQL update query by ID
@@ -86,6 +87,5 @@ router.get('/persons/delete/:id', (req, res) => {
         res.json({ message: 'Employee deleted successfully' });
     });
 });
-
-module.exports = router
-;
+// Export the router object
+module.exports = router;
